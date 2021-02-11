836a428e2ff983196486e381d28f28d4f3253fc6
undersquire <55647897+undersquire@users.noreply.github.com>
2021-02-10 18:28:22 -0800

    Update SetStdInEcho.h

--- stdin/SetStdInEcho.h
+++ stdin/SetStdInEcho.h
@@ -5,33 +5,20 @@
 #include <unistd.h>
 #endif
 
-typedef enum {false, true} bool;
-
-void SetStdInEcho(bool EchoState)
+void setecho(int state)
 {
 #ifdef WIN32
-
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); 
     DWORD mode;
     GetConsoleMode(hStdin, &mode);
-
-    if( !EchoState )
-        mode &= ~ENABLE_ECHO_INPUT;
-    else
-        mode |= ENABLE_ECHO_INPUT;
-
+    if(!state) mode &= ~ENABLE_ECHO_INPUT;
+    else mode |= ENABLE_ECHO_INPUT;
     SetConsoleMode(hStdin, mode );
-
 #else /* same for most platforms */
-
     struct termios tty;
     tcgetattr(STDIN_FILENO, &tty);
-    if( !EchoState )
-        tty.c_lflag &= ~(0u | ECHO);
-    else
-        tty.c_lflag |= (0u | ECHO);
-
+    if(!state) tty.c_lflag &= ~(0u | ECHO);
+    else tty.c_lflag |= (0u | ECHO);
     (void) tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-
 #endif
 }
